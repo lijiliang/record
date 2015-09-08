d466a8ee37accf3a087e6844adf92f7c651198b7
jiliang <jiliang@liang.local>
2015-09-08 13:57:34 +0800

    add 前端技能汇总

--- node/nodeapi/fs/read_file.js
+++ node/nodeapi/fs/read_file.js
@@ -1,5 +1,14 @@
 var fs = require("fs");
 
 //以同步方式读取文件
-var data = fs.readFileSync("nex.txt");
-console.log(data.toString());
+/*var data = fs.readFileSync("nex.txt");
+console.log(data.toString());*/
+
+fs.open("new.txt","r",function(err,fd){
+	fs.fstat(fd, function(err,stat){
+		var buf = new Buffer(stat.size);
+		fs.read(fd,buf,0,buf.length,0,function(err,bytesRead,buffer){
+			console.log(buffer.toString());
+		})
+	})
+})
