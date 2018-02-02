beb7a796bb88d16d80954b066e5e7cea71b50ae7
Benson <benson@Benson.local>
2018-02-02 15:26:47 +0800

    feat: 登入登出

--- vue/vue-mongodb-mall/server/routes/users.js
+++ vue/vue-mongodb-mall/server/routes/users.js
@@ -1,13 +1,63 @@
 var express = require('express');
 var router = express.Router();
+var User = require('./../models/user')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/test', function(req, res, next) {
-  res.send('test asdf sadf');
-});
+// 登录接口
+router.post('/login', function(req, res, next){
+  var param = {
+    userName: req.body.userName,
+    userPwd: req.body.userPwd
+  }
+  User.findOne(param, function(err, doc){
+    if(err){
+      res.json({
+        status: '1',
+        msg: err.message
+      })
+    }else{
+      if(doc){
+        // 存cookie
+        res.cookie("userId", doc.userId, {
+          path: '/',
+          maxAge: 1000 * 60 * 60
+        });
+        // 存session
+        // req.session.user = doc;
+
+        res.json({
+          status: '0',
+          msg: '',
+          result: {
+            userName: doc.userName
+          }
+        })
+      }else{
+        res.json({
+          status: '1',
+          msg: '帐号及密码错误',
+        })
+      }
+    }
+  })
+})
+
+// 登出
+router.post('/logout', function(req, res, next){
+  // 设置cookie过期
+  res.cookie('userId', "", {
+    path: '/',
+    maxAge: -1
+  })
+  res.json({
+    status: '0',
+    msg: '',
+    result: ''
+  })
+})
 
 module.exports = router;
