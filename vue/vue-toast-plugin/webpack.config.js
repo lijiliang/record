4ce575c53b3010e78b112b91d118880ccd4fd635
Benson <benson@Benson.local>
2018-02-06 17:18:42 +0800

    add: vue插件

---
+++ vue/vue-toast-plugin/webpack.config.js
@@ -0,0 +1,32 @@
+let path = require('path')
+module.exports = {
+  entry: './src/lib/index.js',
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'vue-toast.js',
+    libraryTarget: 'umd',
+    library: 'VueToast'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            scss: 'style-loader!css-loader!sass-loader'
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: path.join(__dirname, 'src'),
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+
+  ]
+}
---
+++ vue/vue-toast-plugin/发布到npm.md
@@ -0,0 +1,10 @@
+## 发布到npmjs.org
+```
+npm adduser  // 然后输入用户名、密码
+
+npm whoami   // 查看当前用户是谁
+
+npm publish  // 发布
+```
+
+https://github.com/JackySoft/vue-toast
