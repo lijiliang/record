eb236bdcb2f3f34d0840b63e2594f2d42ab453eb
benson <sucaiweb@163.com>
2020-03-25 11:02:07 +0800

    feat: vue项目初始化及配置jest

--- tests/jest-vue/jest.config.js
+++ tests/jest-vue/jest.config.js
@@ -3,7 +3,8 @@ module.exports = {
 
   // 以下配置是自定义配置，相同的会覆盖上面的配置
   testPathIgnorePatterns: [
-    '\.eslintrc\.js'
+    // '\.eslintrc\.js'
+    '.eslintrc.js'
   ],
   testMatch: [
     '**/__tests__/**/*.(js|jsx|ts|tsx)'
