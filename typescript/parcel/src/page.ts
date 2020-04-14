5b03b91036a76db794a773f7c741f30a26dafd30
benson <sucaiweb@163.com>
2020-04-14 17:55:19 +0800

    feat: keyof

--- typescript/parcel/src/page.ts
+++ typescript/parcel/src/page.ts
@@ -1,7 +1,33 @@
 import $ from 'jquery'
 
 $(function () {
-  console.log('sdf')
   $('body').html('<div>1234</div>')
   new $.fn.init()
 })
+
+interface Person {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+// type T = 'name'
+// key: 'name'
+// Person['name']
+
+class Teacher {
+  constructor(private info: Person) { }
+
+  // 泛型中的keyof ts 类型保护
+  getInfo<T extends keyof Person>(key: T): Person[T] {
+    return this.info[key]
+
+  }
+}
+const teacher = new Teacher({
+  name: 'LI',
+  age: 28,
+  gender: 'male'
+})
+const test = teacher.getInfo('age')
+console.log(test)
